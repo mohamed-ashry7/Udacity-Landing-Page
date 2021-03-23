bec2718420cbc55f0dce7595a84e463217c71cf5
Mohamed Ashry <mohamedelashry7777@gmail.com>
2021-03-23 14:42:19 +0200

    updating the readme

--- README.md
+++ README.md
@@ -1,13 +1,10 @@
 # Landing Page Project
 
-## Table of Contents
+This an interactive page that aims to build a web page of multiple sections and use JS, CSS, and Html to create a navigation menu and deliver a good UI/UX. 
 
-* [Instructions](#instructions)
+## Resources
+This project is implemented just by using JS, CSS, and Html without any external library. We used getbootstrap to add a nice UI for the nav elements. 
 
-## Instructions
+## View the project
 
-The starter project has some HTML and CSS styling to display a static version of the Landing Page project. You'll need to convert this project from a static project to an interactive one. This will require modifying the HTML and CSS files, but primarily the JavaScript file.
-
-To get started, open `js/app.js` and start building out the app's functionality
-
-For specific, detailed instructions, look at the project instructions in the Udacity Classroom.
+To view the project please click here. 
--- js/app.js
+++ js/app.js
@@ -1,113 +1,65 @@
-/**
- * 
- * Manipulating the DOM exercise.
- * Exercise programmatically builds navigation,
- * scrolls to anchors from navigation,
- * and highlights section in viewport upon scrolling.
- * 
- * Dependencies: None
- * 
- * JS Version: ES2015/ES6
- * 
- * JS Standard: ESlint
- * 
-*/
-document.addEventListener('DOMContentLoaded',()=>{
 
+
+
+document.addEventListener('DOMContentLoaded',()=>{
+    // Populating the nav bar elements from the sections
     const allSections = document.querySelectorAll('section'); 
     const navBar = document.querySelector('#navbar__list')
     allSections.forEach(sec => {
         const elem = document.createElement('li');
-        const a = document.createElement('a')
-        a.textContent=`${sec.dataset.nav}    `
-        a.setAttribute('href',`#${sec.id}`)
-        elem.appendChild(a)
-        elem.setAttribute("data-nav",sec.id)
+        const a = document.createElement('a');
+        a.textContent=`${sec.dataset.nav}    `;
+        a.setAttribute('href',`#${sec.id}`);
+        elem.appendChild(a);
+        elem.setAttribute("data-nav",sec.id);
         navBar.appendChild(elem); 
     }); 
     
     
 
-
+    
     const deactivateAllObeject= (objects)=>{
         objects.forEach((ll) =>{
-            ll.classList.remove('active')
+            ll.classList.remove('active');
         })
     }
+
     navBar.addEventListener('click',(event)=>{
         
-        event.preventDefault()
-        const sectionSelected = document.getElementById(event.target.parentElement.dataset.nav)
-        sectionSelected.scrollIntoView({behavior: 'smooth', block: 'center'})
+        event.preventDefault();
+        const sectionSelected = document.getElementById(event.target.parentElement.dataset.nav);
+        sectionSelected.scrollIntoView({behavior: 'smooth', block: 'center'});
 
     });
     
 
     const ScrollHandler = (entries,observer)=>{
-        deactivateAllObeject(allSections)
-        const allItems =navBar.querySelectorAll('li')
-        deactivateAllObeject(allItems)
+        deactivateAllObeject(allSections);
+        const allItems =navBar.querySelectorAll('li');
+        deactivateAllObeject(allItems);
         entries.forEach((e)=>{
             if(e.intersectionRatio>0.2){
-                e.target.classList.add('active')
+                // Activating the element into the view port. 
+                e.target.classList.add('active');
                 document.querySelector(`[data-nav=${e.target.id}]`).classList.add('active');
 
             }
         })
     }
-    let ScrollOptions = {
+    const ScrollOptions = {
         root: null, 
         rootMargin: '0px', 
         threshold: 1
       };
     
-    let observer = new IntersectionObserver(ScrollHandler, ScrollOptions);
+    const observer = new IntersectionObserver(ScrollHandler, ScrollOptions);
     allSections.forEach((sec)=>{
-        observer.observe(sec)
-    })
+        observer.observe(sec);
+    });
 
 })
 
 
-/**
- * Define Global Variables
- * 
-*/
 
-
-/**
- * End Global Variables
- * Start Helper Functions
- * 
-*/
-
-
-
-/**
- * End Helper Functions
- * Begin Main Functions
- * 
-*/
-
-// build the nav
-
-
-// Add class 'active' to section when near top of viewport
-
-
-// Scroll to anchor ID using scrollTO event
-
-
-/**
- * End Main Functions
- * Begin Events
- * 
-*/
-
-// Build menu 
-
-// Scroll to section on link click
-
-// Set sections as active
 
 
